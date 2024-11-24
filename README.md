59928133182557814b71d60fcb781463262b335e
André Barros <andrecbarros10@gmail.com>
2024-11-24 20:27:11 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um clone do https://tabnews.com.br utilizado no curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-  return <h1>Tudo dara certo no final, se não deu, é porque não chegou o fim.</h1>
+  return (
+    <h1>Tudo dara certo no final, se não deu, é porque não chegou o fim.</h1>
+  );
 }
 
 export default Home;
